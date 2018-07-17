ffd60d4720e32948ad8f20d769a0fcedbd67b7ca
khalid jamal <kh.jamal95@gmail.com>
2018-07-17 08:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
 
-const firstInitial = currentUser[0]
-const restOfName = currentUser.slice(1);
+
 const shortGreeting = `Welcome, ${firstInitial}!`
