cd484c027bf6e503e4033553447829ef9e00cd9e
khalid jamal <kh.jamal95@gmail.com>
2018-07-17 08:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const excitedWelcomeMessage = welcomeMessage.toUpperCase()
 
 const firstInitial = currentUser[0];
 const restOfName = currentUser.slice(1);
-const shortGreeting = `Welcome, ${firstInitial}`
+const shortGreeting = `Welcome, ${firstInitial}!`
