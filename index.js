fafff2d3c2e9d2649d318341cbd1f7047e793834
khalid jamal <kh.jamal95@gmail.com>
2018-07-17 08:31:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 const currentUser ="Khalid Jamal"
 
-let welcomeMessage =`Welcome to Flatbook, ${currentUser}!'
+const welcomeMessage ='Welcome to Flatbook, ${currentUser}!'
 
 let excitedWelcomeMessage = "Welcome to Flatbook, "+ currentUser + '!'
